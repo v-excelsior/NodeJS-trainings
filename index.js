7f1edc45084b34e587f1511789391a9401cde51c
vakyla98 <vakyla98@ukr.net>
2020-08-01 21:09:05 +0300

    Check how work OS module

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const http = require("http");
+const os = require("os");
+
 http.createServer(function (request, response) {
-
     response.end("Hello NodeJS!");
-
 }).listen(3000, "127.0.0.1", function () {
     console.log("Сервер начал прослушивание запросов на порту 3000");
 });
+
+let userName = os.userInfo().username;
+console.log(userName);
